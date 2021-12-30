ddb0d6c33953f44519bb06a42cc9f8abb4973399
stevenkreutzer <steven.kreutzer@yahoo.com>
2021-12-30 18:44:23 -0500

    Added sample method headers

--- src/main/java/com/example/project/Warmup.java
+++ src/main/java/com/example/project/Warmup.java
@@ -1,4 +1,16 @@
 public class Warmup
 {
+
+	public static int[] createRandomIntArray(int i, int j, int k)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
   // to do
+
+	public static Integer smallest(int[] is)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
