18800b0777f9c9fe5470c6e0feddcd712dd34c70
analyticone <steven.kreutzer@yahoo.com>
2023-01-20 12:16:16 -0500

    Add files via upload

---
+++ Warmup.java
@@ -0,0 +1,23 @@
+public class Warmup
+{
+
+	public static int[] createRandomIntArray(int i, int j, int k)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+  // to do
+
+	public static Integer smallest(int[] is)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Integer indexOfSmallest(int[] is)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
